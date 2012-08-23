b319c85a8f17f38eb61d09b22f06e6c74e94e55f
Daniel Holth <dholth@fastmail.fm>
2012-08-22 22:03:39 -0400

    signatures.py py3 updates

--- wheel/signatures.py
+++ wheel/signatures.py
@@ -2,6 +2,8 @@
 Create and verify jws-js format Ed25519 signatures.
 """
 
+__all__ = [ 'sign', 'verify' ]
+
 import json
 try:
     import ed25519ll
@@ -9,6 +11,20 @@ except ImportError:
     ed25519ll = None
 from wheel.util import urlsafe_b64decode, urlsafe_b64encode
 
+try:
+    unicode
+    def native(s):
+        return s
+    def binary(s):
+        return s
+except NameError:
+    def native(s):
+        if isinstance(s, bytes):
+            return s.decode('latin1')
+        return s
+    def binary(s):
+        if isinstance(s, str):
+            return s.encode('latin1')
 
 def sign(payload, keypair):
     """Return a JWS-JS format signature given a JSON-serializable payload and 
@@ -16,18 +32,18 @@ def sign(payload, keypair):
     header = {"typ": "JWT",
               "alg": "Ed25519",
               "key": {"alg": "Ed25519",
-                      "vk": urlsafe_b64encode(keypair.vk)}}
+                      "vk": native(urlsafe_b64encode(keypair.vk))}}
     
-    encoded_header = urlsafe_b64encode(json.dumps(header, sort_keys=True)) 
-    encoded_payload = urlsafe_b64encode(json.dumps(payload, sort_keys=True))
-    secured_input = ".".join((encoded_header, encoded_payload))
+    encoded_header = urlsafe_b64encode(binary(json.dumps(header, sort_keys=True)))
+    encoded_payload = urlsafe_b64encode(binary(json.dumps(payload, sort_keys=True)))
+    secured_input = b".".join((encoded_header, encoded_payload))
     sig_msg = ed25519ll.crypto_sign(secured_input, keypair.sk)
     signature = sig_msg[:ed25519ll.SIGNATUREBYTES]
     encoded_signature = urlsafe_b64encode(signature)
     
-    return {"headers": [encoded_header],
-            "payload": encoded_payload,
-            "signatures": [encoded_signature]}
+    return {"headers": [native(encoded_header)],
+            "payload": native(encoded_payload),
+            "signatures": [native(encoded_signature)]}
     
 def verify(jwsjs):
     """Return (decoded headers, payload) if all signatures in jwsjs are
@@ -36,25 +52,29 @@ def verify(jwsjs):
     Caller must decide whether the keys are actually trusted."""
     # XXX forbid duplicate keys in JSON input
     encoded_headers = jwsjs["headers"]
-    encoded_payload = jwsjs["payload"].encode('latin1')
+    encoded_payload = binary(jwsjs["payload"])
     encoded_signatures = jwsjs["signatures"]
     headers = []
     for h, s in zip(encoded_headers, encoded_signatures):
-        h = h.encode('latin1')
-        s = s.encode('latin1')
-        header = json.loads(urlsafe_b64decode(h))
+        h = binary(h)
+        s = binary(s)
+        header = json.loads(native(urlsafe_b64decode(h)))
         assert header["alg"] == "Ed25519"
         assert header["key"]["alg"] == "Ed25519"
-        vk = urlsafe_b64decode(header["key"]["vk"].encode('latin1'))
-        secured_input = ".".join((h, encoded_payload))
-        sig = urlsafe_b64decode(s.encode('latin1'))
+        vk = urlsafe_b64decode(binary(header["key"]["vk"]))
+        secured_input = b".".join((h, encoded_payload))
+        sig = urlsafe_b64decode(s)
         sig_msg = sig+secured_input
-        verified_input = ed25519ll.crypto_sign_open(sig_msg, vk)
+        verified_input = native(ed25519ll.crypto_sign_open(sig_msg, vk))
         verified_header, verified_payload = verified_input.split('.')
-        headers.append(json.loads(urlsafe_b64decode(verified_header)))
+        verified_header = binary(verified_header)
+        decoded_payload = native(urlsafe_b64decode(verified_header))
+        headers.append(json.loads(decoded_payload))
+
+    verified_payload = binary(verified_payload)
 
     # only return header, payload that have passed through the crypto library.
-    payload = json.loads(urlsafe_b64decode(verified_payload))
+    payload = json.loads(native(urlsafe_b64decode(verified_payload)))
 
     return headers, payload
 
@@ -64,7 +84,7 @@ def test():
     # Takes less than 25 milliseconds.
     kp = ed25519ll.crypto_sign_keypair()
     payload = {'test': 'onstartup'}
-    jwsjs = sign(payload, kp)
+    jwsjs = json.loads(json.dumps(sign(payload, kp)))
     verify(jwsjs)
     jwsjs['payload'] += 'x'
     try:
