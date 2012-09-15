6b83b88bbf8bda0eef67505e7e96c9fa0e4cdd40
Daniel Holth <dholth@fastmail.fm>
2012-09-15 12:01:25 -0400

    allow wheel's wheel to be executed directly

--- wheel/__main__.py
+++ wheel/__main__.py
@@ -3,6 +3,12 @@ Wheel command line tool (enable python -m wheel syntax)
 """
 
 def main(): # needed for console script
+    if __package__ == '':
+        # To be able to run 'python wheel-0.9.whl/wheel':
+        import sys
+        import os.path
+        path = os.path.dirname(os.path.dirname(__file__))
+        sys.path[0:0] = [path]
     import wheel.tool
     wheel.tool.main()
 
