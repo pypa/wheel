679cfc61711abd5437ea646d4f2de22384655e45
Daniel Holth <dholth@fastmail.fm>
2012-09-29 21:42:40 -0400

    add a script to complex-dist

--- wheel/test/complex-dist/setup.py
+++ wheel/test/complex-dist/setup.py
@@ -8,10 +8,11 @@ except NameError:
     def u8(s):
         return s.encode('utf-8')
 
-setup(name='complex.dist',
+setup(name='complex-dist',
       version='0.1',
       description=u8('Another testing distribution \N{SNOWMAN}'),
       packages=['complexdist'],
-      extras_require={'simple':['simple.dist']}
+      extras_require={'simple':['simple.dist']},
+      entry_points={'console_scripts':['complex-dist=complexdist:main']}
       )
 
