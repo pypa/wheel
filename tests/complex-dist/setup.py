8004cbe8dc8be834a40717e3d943af3cc28938cd
Alex Grönholm <alex.gronholm@nextday.fi>
2018-04-01 23:22:23 +0300

    Simplified the setup.py files in the example distributions

--- tests/complex-dist/setup.py
+++ tests/complex-dist/setup.py
@@ -1,18 +1,9 @@
 from setuptools import setup
 
-try:
-    unicode
-
-    def u8(s):
-        return s.decode('unicode-escape')
-except NameError:
-    def u8(s):
-        return s
-
 setup(name='complex-dist',
       version='0.1',
-      description=u8('Another testing distribution \N{SNOWMAN}'),
-      long_description=u8('Another testing distribution \N{SNOWMAN}'),
+      description=u'Another testing distribution \N{SNOWMAN}',
+      long_description=u'Another testing distribution \N{SNOWMAN}',
       author="Illustrious Author",
       author_email="illustrious@example.org",
       url="http://example.org/exemplary",
--- tests/extension.dist/setup.py
+++ tests/extension.dist/setup.py
@@ -1,17 +1,8 @@
 from setuptools import setup, Extension
 
-try:
-    unicode
-
-    def u8(s):
-        return s.decode('unicode-escape').encode('utf-8')
-except NameError:
-    def u8(s):
-        return s.encode('utf-8')
-
 setup(name='extension.dist',
       version='0.1',
-      description=u8('A testing distribution \N{SNOWMAN}'),
+      description=u'A testing distribution \N{SNOWMAN}',
       ext_modules=[
           Extension(name='extension',
                     sources=['extension.c'],
