a5428e688825146367c9a220708c6a1875b627bd
Benoit Pierre <benoit.pierre@gmail.com>
2018-10-19 23:18:23 +0200

    fix handling of direct URL requirements with markers
    There must be at least one space between the URL and the `;`
    delimiting the markers.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -7,19 +7,22 @@ def test_pkginfo_to_metadata(tmpdir):
         ('Name', 'spam'),
         ('Version', '0.1'),
         ('Requires-Dist', "pip @ https://github.com/pypa/pip/archive/1.3.1.zip"),
-        ('Requires-Dist', 'pywin32; sys_platform=="win32"'),
+        ('Requires-Dist', 'pywin32 ; sys_platform=="win32"'),
+        ('Requires-Dist', 'foo @ http://host/foo.zip ; sys_platform=="win32"'),
         ('Provides-Extra', 'signatures'),
-        ('Requires-Dist', 'pyxdg; (sys_platform!="win32") and extra == \'signatures\''),
+        ('Requires-Dist', 'pyxdg ; (sys_platform!="win32") and extra == \'signatures\''),
         ('Provides-Extra', 'empty_extra'),
+        ('Provides-Extra', 'extra'),
+        ('Requires-Dist', 'bar @ http://host/bar.zip ; extra == \'extra\''),
         ('Provides-Extra', 'faster-signatures'),
-        ('Requires-Dist', "ed25519ll; extra == 'faster-signatures'"),
+        ('Requires-Dist', "ed25519ll ; extra == 'faster-signatures'"),
         ('Provides-Extra', 'rest'),
-        ('Requires-Dist', "docutils (>=0.8); extra == 'rest'"),
-        ('Requires-Dist', "keyring; extra == 'signatures'"),
-        ('Requires-Dist', "keyrings.alt; extra == 'signatures'"),
+        ('Requires-Dist', "docutils (>=0.8) ; extra == 'rest'"),
+        ('Requires-Dist', "keyring ; extra == 'signatures'"),
+        ('Requires-Dist', "keyrings.alt ; extra == 'signatures'"),
         ('Provides-Extra', 'test'),
-        ('Requires-Dist', "pytest (>=3.0.0); extra == 'test'"),
-        ('Requires-Dist', "pytest-cov; extra == 'test'"),
+        ('Requires-Dist', "pytest (>=3.0.0) ; extra == 'test'"),
+        ('Requires-Dist', "pytest-cov ; extra == 'test'"),
     ]
 
     pkg_info = tmpdir.join('PKG-INFO')
@@ -38,10 +41,14 @@ Provides-Extra: faster-signatures""")
     egg_info_dir.join('requires.txt').write("""\
 pip@https://github.com/pypa/pip/archive/1.3.1.zip
 
+[extra]
+bar @ http://host/bar.zip
+
 [empty+extra]
 
 [:sys_platform=="win32"]
 pywin32
+foo @http://host/foo.zip
 
 [faster-signatures]
 ed25519ll
