f4d3409b3d16d26d144db34cf8b5b93f0ce0c037
Asif Saif Uddin <auvipy@gmail.com>
2019-05-12 20:14:31 +0600

    Dropped Python 3.4

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 def readall(*args):
-    with io.open(os.path.join(here, *args), encoding='utf8') as fp:
-        return fp.read()
+  with io.open(os.path.join(here, *args), encoding='utf8') as fp:
+    return fp.read()
 
 
 README = readall('README.rst')
-metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", readall('wheel', '__init__.py')))
+metadata = dict(re.findall(
+    r"""__([a-z]+)__ = "([^"]+)""", readall('wheel', '__init__.py')))
 
 setup(name='wheel',
       version=metadata['version'],
@@ -30,7 +31,6 @@ setup(name='wheel',
           "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7"
@@ -48,18 +48,18 @@ setup(name='wheel',
       keywords=['wheel', 'packaging'],
       license='MIT',
       packages=find_packages(),
-      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
       extras_require={
           'test': ['pytest >= 3.0.0', 'pytest-cov']
-          },
+      },
       include_package_data=True,
       zip_safe=False,
       entry_points={
           'console_scripts': [
               'wheel=wheel.cli:main'
-              ],
+          ],
           'distutils.commands': [
               'bdist_wheel=wheel.bdist_wheel:bdist_wheel'
-              ]
-          }
+          ]
+      }
       )
