d5969f02fabbcbc9938390a0df6b818661a97210
Alex Grönholm <alex.gronholm@nextday.fi>
2020-06-09 21:06:28 +0300

    Replaced old WheelFile with the proposed new one

--- src/wheel/wheelfile.py
+++ src/wheel/wheelfile.py
@@ -1,169 +1,302 @@
-from __future__ import print_function
-
 import csv
 import hashlib
 import os.path
 import re
-import stat
 import time
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 from collections import OrderedDict
-from distutils import log as logger
+from email.generator import Generator
+from email.message import Message
+from io import StringIO
+from os import PathLike
+from pathlib import Path
+from typing import Optional, Union, Dict, Iterable, NamedTuple, Tuple
 from zipfile import ZIP_DEFLATED, ZipInfo, ZipFile
 
-from wheel.cli import WheelError
-from wheel.util import urlsafe_b64decode, as_unicode, native, urlsafe_b64encode, as_bytes, StringIO
+from . import __version__ as wheel_version
 
-# Non-greedy matching of an optional build number may be too clever (more
-# invalid wheel filenames will match). Separate regex for .dist-info?
-WHEEL_INFO_RE = re.compile(
+_WHEEL_INFO_RE = re.compile(
     r"""^(?P<namever>(?P<name>.+?)-(?P<ver>.+?))(-(?P<build>\d[^-]*))?
      -(?P<pyver>.+?)-(?P<abi>.+?)-(?P<plat>.+?)\.whl$""",
     re.VERBOSE)
 
+WheelMetadata = NamedTuple('WheelMetadata', [
+    ('name', str),
+    ('version', str),
+    ('build_tag', Optional[str]),
+    ('implementation', str),
+    ('abi', str),
+    ('platform', str)
+])
 
-def get_zipinfo_datetime(timestamp=None):
-    # Some applications need reproducible .whl files, but they can't do this without forcing
-    # the timestamp of the individual ZipInfo objects. See issue #143.
-    timestamp = int(os.environ.get('SOURCE_DATE_EPOCH', timestamp or time.time()))
-    return time.gmtime(timestamp)[0:6]
+WheelRecordEntry = NamedTuple('_WheelRecordEntry', [
+    ('hash_algorithm', str),
+    ('hash_value', bytes),
+    ('filesize', int)
+])
 
 
-class WheelFile(ZipFile):
-    """A ZipFile derivative class that also reads SHA-256 hashes from
-    .dist-info/RECORD and checks any read files against those.
-    """
+def parse_filename(filename: str) -> WheelMetadata:
+    parsed_filename = _WHEEL_INFO_RE.match(filename)
+    if parsed_filename is None:
+        raise WheelError('Bad wheel filename {!r}'.format(filename))
 
-    _default_algorithm = hashlib.sha256
+    return WheelMetadata(*parsed_filename.groups()[1:])
 
-    def __init__(self, file, mode='r', compression=ZIP_DEFLATED):
-        basename = os.path.basename(file)
-        self.parsed_filename = WHEEL_INFO_RE.match(basename)
-        if not basename.endswith('.whl') or self.parsed_filename is None:
-            raise WheelError("Bad wheel filename {!r}".format(basename))
 
-        ZipFile.__init__(self, file, mode, compression=compression, allowZip64=True)
+def make_filename(name: str, version: str, build_tag: Union[str, int, None] = None,
+                  impl_tag: str = 'py3', abi_tag: str = 'none', plat_tag: str = 'any') -> str:
+    filename = '{}-{}'.format(name, version)
+    if build_tag is not None:
+        filename = '{}-{}'.format(filename, build_tag)
 
-        self.dist_info_path = '{}.dist-info'.format(self.parsed_filename.group('namever'))
-        self.record_path = self.dist_info_path + '/RECORD'
-        self._file_hashes = OrderedDict()
-        self._file_sizes = {}
+    return '{}-{}-{}-{}.whl'.format(filename, impl_tag, abi_tag, plat_tag)
+
+
+class WheelError(Exception):
+    pass
+
+
+class WheelFile:
+    __slots__ = ('generator', 'root_is_purelib', '_metadata', '_mode', '_zip', '_data_path',
+                 '_dist_info_path', '_record_path', '_record_entries', '_exclude_archive_names')
+
+    # dist-info file names ignored for hash checking/recording
+    _exclude_filenames = ('RECORD', 'RECORD.jws', 'RECORD.p7s')
+    _default_hash_algorithm = 'sha256'
+
+    def __init__(self, path: Union[str, PathLike], mode: str = 'r', *,
+                 compression: int = ZIP_DEFLATED, generator: Optional[str] = None,
+                 root_is_purelib: bool = True):
+        path = str(path)
+        self.generator = generator or 'Wheel {}'.format(wheel_version)
+        self.root_is_purelib = root_is_purelib
+        self._mode = mode
+        self._metadata = parse_filename(path)
+        self._data_path = '{meta.name}-{meta.version}.data'.format(meta=self._metadata)
+        self._dist_info_path = '{meta.name}-{meta.version}.dist-info'.format(meta=self._metadata)
+        self._record_path = self._dist_info_path + '/RECORD'
+        self._exclude_archive_names = frozenset(self._dist_info_path + '/' + fname
+                                                for fname in self._exclude_filenames)
+        self._zip = ZipFile(path, mode, compression=compression)
+        self._record_entries = OrderedDict()  # type: Dict[str, WheelRecordEntry]
+
         if mode == 'r':
-            # Ignore RECORD and any embedded wheel signatures
-            self._file_hashes[self.record_path] = None, None
-            self._file_hashes[self.record_path + '.jws'] = None, None
-            self._file_hashes[self.record_path + '.p7s'] = None, None
+            self._read_record()
+        elif mode != 'w':
+            raise ValueError("mode must be either 'r' or 'w'")
 
-            # Fill in the expected hashes by reading them from RECORD
+    @property
+    def path(self) -> Path:
+        return Path(self._zip.filename)
+
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @property
+    def metadata(self) -> WheelMetadata:
+        return self._metadata
+
+    def close(self) -> None:
+        try:
+            if self.mode == 'w':
+                self._write_wheelfile()
+                self._write_record()
+        except BaseException:
+            self._zip.close()
+            if self.mode == 'w':
+                os.unlink(self._zip.filename)
+
+            raise
+        finally:
             try:
-                record = self.open(self.record_path)
-            except KeyError:
-                raise WheelError('Missing {} file'.format(self.record_path))
+                self._zip.close()
+            finally:
+                self._record_entries.clear()
 
-            with record:
-                for line in record:
-                    line = line.decode('utf-8')
-                    path, hash_sum, size = line.rsplit(u',', 2)
-                    if hash_sum:
-                        algorithm, hash_sum = hash_sum.split(u'=')
-                        try:
-                            hashlib.new(algorithm)
-                        except ValueError:
-                            raise WheelError('Unsupported hash algorithm: {}'.format(algorithm))
+    def __enter__(self) -> 'WheelFile':
+        return self
 
-                        if algorithm.lower() in {'md5', 'sha1'}:
-                            raise WheelError(
-                                'Weak hash algorithm ({}) is not permitted by PEP 427'
-                                .format(algorithm))
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
 
-                        self._file_hashes[path] = (
-                            algorithm, urlsafe_b64decode(hash_sum.encode('ascii')))
+    @staticmethod
+    def _get_zipinfo_datetime(timestamp: float) -> Tuple[int, int, int, int, int, int]:
+        # Some applications need reproducible .whl files, but they can't do this without forcing
+        # the timestamp of the individual ZipInfo objects. See issue #143.
+        timestamp = int(os.environ.get('SOURCE_DATE_EPOCH', timestamp))
+        return time.gmtime(timestamp)[0:6]
 
-    def open(self, name_or_info, mode="r", pwd=None):
-        def _update_crc(newdata, eof=None):
-            if eof is None:
-                eof = ef._eof
-                update_crc_orig(newdata)
-            else:  # Python 2
-                update_crc_orig(newdata, eof)
+    def write_file(self, archive_name: str, contents: Union[bytes, str, PathLike]) -> None:
+        timestamp = time.time()
+        if isinstance(contents, bytes):
+            data = contents
+        elif isinstance(contents, str):
+            data = contents.encode('utf-8')
+        elif isinstance(contents, PathLike):
+            path = Path(contents)
+            timestamp = path.stat().st_mtime
+            data = path.read_bytes()
+        else:
+            raise TypeError('contents must be a str, bytes or a path-like object')
 
-            running_hash.update(newdata)
-            if eof and running_hash.digest() != expected_hash:
-                raise WheelError("Hash mismatch for file '{}'".format(native(ef_name)))
+        if archive_name not in self._exclude_archive_names:
+            hash_digest = hashlib.new(self._default_hash_algorithm, data).digest()
+            self._record_entries[archive_name] = WheelRecordEntry(
+                self._default_hash_algorithm, hash_digest, len(data))
 
-        ef_name = as_unicode(name_or_info.filename if isinstance(name_or_info, ZipInfo)
-                             else name_or_info)
-        if mode == 'r' and not ef_name.endswith('/') and ef_name not in self._file_hashes:
-            raise WheelError("No hash found for file '{}'".format(native(ef_name)))
+        zinfo = ZipInfo(archive_name, date_time=self._get_zipinfo_datetime(timestamp))
+        zinfo.external_attr = 0o664 << 16
+        self._zip.writestr(zinfo, data)
 
-        ef = ZipFile.open(self, name_or_info, mode, pwd)
-        if mode == 'r' and not ef_name.endswith('/'):
-            algorithm, expected_hash = self._file_hashes[ef_name]
-            if expected_hash is not None:
-                # Monkey patch the _update_crc method to also check for the hash from RECORD
-                running_hash = hashlib.new(algorithm)
-                update_crc_orig, ef._update_crc = ef._update_crc, _update_crc
+    def write_data_file(self, archive_name: str, contents: Union[bytes, str, PathLike]) -> None:
+        archive_path = self._data_path + '/' + archive_name
+        self.write_file(archive_path, contents)
 
-        return ef
+    def write_metadata_file(self, archive_name: str,
+                            contents: Union[bytes, str, PathLike]) -> None:
+        archive_path = self._dist_info_path + '/' + archive_name
+        self.write_file(archive_path, contents)
 
-    def write_files(self, base_dir):
-        logger.info("creating '%s' and adding '%s' to it", self.filename, base_dir)
+    def write_files_from_directory(self, base_path: Union[str, PathLike]) -> None:
+        base_path = Path(base_path)
+        if not base_path.is_dir():
+            raise WheelError('{} is not a directory'.format(base_path))
+
         deferred = []
-        for root, dirnames, filenames in os.walk(base_dir):
+        for root, dirnames, filenames in os.walk(str(base_path)):
             # Sort the directory names so that `os.walk` will walk them in a
             # defined order on the next iteration.
             dirnames.sort()
+            root_path = base_path / root
             for name in sorted(filenames):
-                path = os.path.normpath(os.path.join(root, name))
-                if os.path.isfile(path):
-                    arcname = os.path.relpath(path, base_dir).replace(os.path.sep, '/')
-                    if arcname == self.record_path:
+                path = root_path / name
+                if path.is_file():
+                    archive_name = str(path.relative_to(base_path))
+                    if archive_name in self._exclude_filenames:
                         pass
                     elif root.endswith('.dist-info'):
-                        deferred.append((path, arcname))
+                        deferred.append((path, archive_name))
                     else:
-                        self.write(path, arcname)
+                        self.write_file(archive_name, path)
 
-        deferred.sort()
-        for path, arcname in deferred:
-            self.write(path, arcname)
+        for path, archive_name in sorted(deferred):
+            self.write_file(archive_name, path)
 
-    def write(self, filename, arcname=None, compress_type=None):
-        with open(filename, 'rb') as f:
-            st = os.fstat(f.fileno())
-            data = f.read()
+    def read_file(self, archive_name: str) -> bytes:
+        try:
+            contents = self._zip.read(archive_name)
+        except KeyError:
+            raise WheelError('File {} not found'.format(archive_name)) from None
 
-        zinfo = ZipInfo(arcname or filename, date_time=get_zipinfo_datetime(st.st_mtime))
-        zinfo.external_attr = (stat.S_IMODE(st.st_mode) | stat.S_IFMT(st.st_mode)) << 16
-        zinfo.compress_type = compress_type or self.compression
-        self.writestr(zinfo, data, compress_type)
+        if archive_name in self._record_entries:
+            entry = self._record_entries[archive_name]
+            if len(contents) != entry.filesize:
+                raise WheelError('{}: file size mismatch: {} bytes in RECORD, {} bytes in archive'
+                                 .format(archive_name, entry.filesize, len(contents)))
 
-    def writestr(self, zinfo_or_arcname, bytes, compress_type=None):
-        ZipFile.writestr(self, zinfo_or_arcname, bytes, compress_type)
-        fname = (zinfo_or_arcname.filename if isinstance(zinfo_or_arcname, ZipInfo)
-                 else zinfo_or_arcname)
-        logger.info("adding '%s'", fname)
-        if fname != self.record_path:
-            hash_ = self._default_algorithm(bytes)
-            self._file_hashes[fname] = hash_.name, native(urlsafe_b64encode(hash_.digest()))
-            self._file_sizes[fname] = len(bytes)
+            computed_hash = hashlib.new(entry.hash_algorithm, contents).digest()
+            if computed_hash != entry.hash_value:
+                raise WheelError(
+                    '{}: hash mismatch: {} in RECORD, {} computed from current file contents'
+                    .format(archive_name, urlsafe_b64encode(entry.hash_value).decode('ascii'),
+                            urlsafe_b64encode(computed_hash).decode('ascii')))
 
-    def close(self):
-        # Write RECORD
-        if self.fp is not None and self.mode == 'w' and self._file_hashes:
-            data = StringIO()
-            writer = csv.writer(data, delimiter=',', quotechar='"', lineterminator='\n')
-            writer.writerows((
-                (
-                    fname,
-                    algorithm + "=" + hash_,
-                    self._file_sizes[fname]
+        return contents
+
+    def read_data_file(self, filename: str) -> bytes:
+        return self.read_file(self._data_path + '/' + filename)
+
+    def read_metadata_file(self, filename: str) -> bytes:
+        return self.read_file(self._dist_info_path + '/' + filename)
+
+    def unpack(self, dest_dir: Union[str, PathLike],
+               archive_names: Union[str, Iterable[str], None] = None) -> None:
+        base_path = Path(dest_dir)
+        if not base_path.is_dir():
+            raise WheelError('{} is not a directory'.format(base_path))
+
+        if archive_names is None:
+            filenames = self._zip.infolist()
+        elif isinstance(archive_names, str):
+            filenames = [self._zip.getinfo(archive_names)]
+        else:
+            filenames = [self._zip.getinfo(fname) for fname in archive_names]
+
+        for zinfo in filenames:
+            entry = None  # type: Optional[WheelRecordEntry]
+            if zinfo.filename in self._record_entries:
+                entry = self._record_entries[zinfo.filename]
+
+            path = base_path.joinpath(zinfo.filename)
+            with self._zip.open(zinfo) as infile, path.open('wb') as outfile:
+                hash_ = hashlib.new(entry.hash_algorithm) if entry else None
+                while True:
+                    data = infile.read(1024 * 1024)
+                    if data:
+                        if hash_:
+                            hash_.update(data)
+
+                        outfile.write(data)
+                    else:
+                        break
+
+            if hash_ is not None and entry is not None and hash_.digest() != entry.hash_value:
+                raise WheelError(
+                    '{}: hash mismatch: {} in RECORD, {} computed from current file contents'
+                    .format(zinfo.filename, urlsafe_b64encode(entry.hash_value).decode('ascii'),
+                            urlsafe_b64encode(hash_.digest()).decode('ascii'))
                 )
-                for fname, (algorithm, hash_) in self._file_hashes.items()
-            ))
-            writer.writerow((format(self.record_path), "", ""))
-            zinfo = ZipInfo(native(self.record_path), date_time=get_zipinfo_datetime())
-            zinfo.compress_type = self.compression
-            zinfo.external_attr = 0o664 << 16
-            self.writestr(zinfo, as_bytes(data.getvalue()))
 
-        ZipFile.close(self)
+    def _read_record(self) -> None:
+        self._record_entries.clear()
+        contents = self.read_metadata_file('RECORD').decode('utf-8')
+        for line in contents.split('\n'):
+            path, hash_digest, filesize = line.rsplit(',', 2)
+            if hash_digest:
+                algorithm, hash_digest = hash_digest.split('=')
+                try:
+                    hashlib.new(algorithm)
+                except ValueError:
+                    raise WheelError('Unsupported hash algorithm: {}'.format(algorithm))
+
+                if algorithm.lower() in {'md5', 'sha1'}:
+                    raise WheelError(
+                        'Weak hash algorithm ({}) is not permitted by PEP 427'
+                        .format(algorithm))
+
+                self._record_entries[path] = WheelRecordEntry(
+                    algorithm, urlsafe_b64decode(hash_digest), int(filesize))
+
+    def _write_record(self) -> None:
+        data = StringIO()
+        writer = csv.writer(data, delimiter=',', quotechar='"', lineterminator='\n')
+        writer.writerows([
+            (fname,
+             entry.hash_algorithm + "=" + urlsafe_b64encode(entry.hash_value).decode('ascii'),
+             entry.filesize)
+            for fname, entry in self._record_entries.items()
+        ])
+        writer.writerow((self._record_path, "", ""))
+        self.write_metadata_file('RECORD', data.getvalue())
+
+    def _write_wheelfile(self) -> None:
+        msg = Message()
+        msg['Wheel-Version'] = '1.0'  # of the spec
+        msg['Generator'] = self.generator
+        msg['Root-Is-Purelib'] = str(self.root_is_purelib).lower()
+        if self.metadata.build_tag is not None:
+            msg['Build'] = self.metadata.build_tag
+
+        for impl in self.metadata.implementation.split('.'):
+            for abi in self.metadata.abi.split('.'):
+                for plat in self.metadata.platform.split('.'):
+                    msg['Tag'] = '-'.join((impl, abi, plat))
+
+        buffer = StringIO()
+        Generator(buffer, maxheaderlen=0).flatten(msg)
+        self.write_metadata_file('WHEEL', buffer.getvalue())
+
+    def __repr__(self):
+        return '{}({!r}, {!r})'.format(self.__class__.__name__, self.path, self.mode)
