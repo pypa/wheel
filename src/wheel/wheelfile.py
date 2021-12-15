21a73b3c3a92c8f631ac80d29bba6837594820a0
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-15 15:37:34 -0800

    Update src/wheel/wheelfile.py
    Co-authored-by: Alex Grönholm <alex.gronholm@nextday.fi>

--- src/wheel/wheelfile.py
+++ src/wheel/wheelfile.py
@@ -12,14 +12,7 @@ from zipfile import ZIP_DEFLATED, ZipInfo, ZipFile
 
 from wheel.cli import WheelError
 from wheel.util import (
-    urlsafe_b64decode,
-    as_unicode,
-    native,
-    urlsafe_b64encode,
-    as_bytes,
-    StringIO,
-    TextIOWrapper
-)
+    urlsafe_b64decode, as_unicode, native, urlsafe_b64encode, as_bytes, StringIO, TextIOWrapper)
 
 # Non-greedy matching of an optional build number may be too clever (more
 # invalid wheel filenames will match). Separate regex for .dist-info?
