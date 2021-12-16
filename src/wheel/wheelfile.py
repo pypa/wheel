267c3dd2315062873645bb7b00b891619592b32e
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-15 16:11:40 -0800

    Address agronholm's review comments

--- src/wheel/wheelfile.py
+++ src/wheel/wheelfile.py
@@ -2,17 +2,18 @@ from __future__ import print_function
 
 import csv
 import hashlib
-from io import TextIOWrapper
 import os.path
 import re
 import stat
 import time
+
 from collections import OrderedDict
 from distutils import log as logger
+from io import TextIOWrapper
 from zipfile import ZIP_DEFLATED, ZipInfo, ZipFile
 
 from wheel.cli import WheelError
-from wheel.util import (urlsafe_b64decode, as_unicode, native, urlsafe_b64encode, as_bytes, StringIO)
+from wheel.util import urlsafe_b64decode, as_unicode, native, urlsafe_b64encode, as_bytes, StringIO
 
 # Non-greedy matching of an optional build number may be too clever (more
 # invalid wheel filenames will match). Separate regex for .dist-info?
