9e1088c3a32ddb414258116e7002a667a15ef2f5
Alex Grönholm <alex.gronholm@nextday.fi>
2021-12-17 01:02:49 +0200

    Use a unicode newline arg

--- src/wheel/wheelfile.py
+++ src/wheel/wheelfile.py
@@ -61,7 +61,7 @@ class WheelFile(ZipFile):
                 raise WheelError('Missing {} file'.format(self.record_path))
 
             with record:
-                for line in csv.reader(TextIOWrapper(record, newline='')):
+                for line in csv.reader(TextIOWrapper(record, newline=u'')):
                     path, hash_sum, size = line
                     if not hash_sum:
                         continue
