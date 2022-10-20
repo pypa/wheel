16c90f46e82891e44fe570ce619badef4a52b631
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-10-20 10:07:42 +0100

    Remove test dependent on Setuptools deprecation

--- tests/test_bdist_wheel.py
+++ tests/test_bdist_wheel.py
@@ -82,20 +82,6 @@ def test_licenses_default(dummy_dist, monkeypatch, tmpdir):
         assert set(wf.namelist()) == DEFAULT_FILES | license_files
 
 
-def test_licenses_deprecated(dummy_dist, monkeypatch, tmpdir):
-    dummy_dist.join("setup.cfg").write("[metadata]\nlicense_file=licenses/DUMMYFILE")
-    monkeypatch.chdir(dummy_dist)
-    prog = [sys.executable, "-W", "default"]
-    build_log = subprocess.check_output(
-        [*prog, "setup.py", "bdist_wheel", "-b", str(tmpdir), "--universal"],
-        stderr=subprocess.STDOUT,
-    )
-    assert b"license_file parameter is deprecated" in build_log
-    with WheelFile("dist/dummy_dist-1.0-py2.py3-none-any.whl") as wf:
-        license_files = {"dummy_dist-1.0.dist-info/DUMMYFILE"}
-        assert set(wf.namelist()) == DEFAULT_FILES | license_files
-
-
 @pytest.mark.parametrize(
     "config_file, config",
     [
