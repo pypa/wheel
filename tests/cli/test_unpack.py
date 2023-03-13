933d556f34ee84833c5d1a02d2cccc66c2a6bebe
Alex Grönholm <alex.gronholm@nextday.fi>
2023-03-13 18:32:19 +0200

    Fixed EncodingWarning

--- tests/cli/test_unpack.py
+++ tests/cli/test_unpack.py
@@ -24,7 +24,7 @@ def test_unpack(wheel_paths, tmp_path):
 def test_unpack_executable_bit(tmp_path):
     wheel_path = tmp_path / "test-1.0-py3-none-any.whl"
     script_path = tmp_path / "script"
-    script_path.write_text("test script")
+    script_path.write_bytes(b"test script")
     script_path.chmod(0o777)
     with WheelFile(wheel_path, "w") as wf:
         wf.write(str(script_path), "script")
