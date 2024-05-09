1e00742acc9fb33f6e71460c3844c2b66532af7f
Alex Grönholm <alex.gronholm@nextday.fi>
2024-05-10 00:11:03 +0300

    Revert "Apply new ruff/pyupgrade rule UP032 (#617)"
    This reverts commit 16206e67f161135cf964eb069296dcee994e4db3.

--- src/wheel/vendored/packaging/markers.py
+++ src/wheel/vendored/packaging/markers.py
@@ -166,7 +166,7 @@ def _evaluate_markers(markers: MarkerList, environment: Dict[str, str]) -> bool:
 
 
 def format_full_version(info: "sys._version_info") -> str:
-    version = f"{info.major}.{info.minor}.{info.micro}"
+    version = "{0.major}.{0.minor}.{0.micro}".format(info)
     kind = info.releaselevel
     if kind != "final":
         version += kind[0] + str(info.serial)
